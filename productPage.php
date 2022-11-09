a05ca48b7f14989c7d30d62cb18cc7a0742e7341
anoshc <anoshc@stud.ntnu.no>
2022-11-09 18:18:21 +0100

    made some changes

--- productPage.php
+++ productPage.php
@@ -25,17 +25,13 @@ $products = $product->getData();
     <?php
 
     if (count($products) > 0) {
-
         for ($i = 0; $i < count($products); $i++) {
     ?>
-            <div class="whole-row">
-                <div class="row"> <?php echo $products[$i]['product_id']; ?></div>
-                <div class="row"> <?php echo $products[$i]['product_name']; ?></div>
-                <div class="row"> <img class="img" src="data/<?php echo $products[$i]['image_name']; ?>" width="100" height="100" /></div>
-                <div class="row"><?php echo $products[$i]['description']; ?></div>
-                <div class="row"> <?php echo $products[$i]['price'] . "<br>"; ?></div>
-            </div>
+            <img class="img" src="data/<?php echo $products[$i]['image_name']; ?>" width="100" height="100" /></div>
     <?php
+            echo $products[$i]['product_name'];
+            echo $products[$i]['description'];
+            echo $products[$i]['price'] . "<br>";
         }
     }
 
@@ -50,8 +46,17 @@ $products = $product->getData();
 
     ?>
 
+<form action="productPage.php" method="post">
+        <div class="qty-form">
+            <label for="quantity">Quantity</label>
+            <input type="number" name="quantity" id="quantity">
+        </div>
+        <div class="qty-form">
+            <button type="submit" class="submit-btn" name="submit">Submit</button>
+        </div>
+    </form>
+
 
 
 </body>
-
 </html>
